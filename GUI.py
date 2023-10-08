eb6b1cc2dddd866d14760640d9a43d1c4f31268e
Rifxt01 <145126679+Rifxt01@users.noreply.github.com>
2023-10-09 00:00:10 +0800

    GUI.py
    Updated with button which displays chart. But has a placeholder sample chart for now

--- GUI.py
+++ GUI.py
@@ -1,17 +1,30 @@
 import datetime
+import WebScraping
+import cleaningtest
 from tkinter import *
 import tkinter.messagebox as mb
 from tkinter import ttk
+from PIL import ImageTk, Image
 
 headlabelfont = ("Noto Sans CJK TC", 15, 'bold')
 labelfont = ('Garamond', 14)
 entryfont = ('Garamond', 12)
 
 main = Tk()
-main.title('Car Price Records')
+main.title('Car Price History')
 main.geometry('1000x700')
 main.resizable()
 
+def show_image(Imagefile):
+    image = ImageTk.PhotoImage(file=Imagefile)
+    label = (Label(right_frame, image=image))
+    label.image = image
+    label.grid(row=0, column=0, padx=5, pady=5)
+    return label
+
+
+# load image
+#image = ImageTk.PhotoImage(Image.open("Chart.png"))
 # Creating the background and foreground color variables
 lf_bg = 'DeepSkyBlue' # bg color for the left_frame
 cf_bg = 'Aqua' # bg color for the center_frame
@@ -29,12 +42,15 @@ right_frame = Frame(main, bg="Gray35")
 right_frame.place(relx=0.4, y=30, relheight=1, relwidth=0.6)
 
 # Placing components in the left frame
-Label(left_frame, text="Vehicle Name", font=labelfont, bg=lf_bg).place(relx=0.1, rely=0.05)
-Label(left_frame, text="Vehicle Model", font=labelfont, bg=lf_bg).place(relx=0.1, rely=0.18)
-Label(left_frame, text="Vehicle Colour", font=labelfont, bg=lf_bg).place(relx=0.1, rely=0.31)
-Label(left_frame, text="Category", font=labelfont, bg=lf_bg).place(relx=0.1, rely=0.44)
-Label(left_frame, text="Current Price", font=labelfont, bg=lf_bg).place(relx=0.1, rely=0.57)
-Label(left_frame, text="Whatever variable", font=labelfont, bg=lf_bg).place(relx=0.1, rely=0.7)
+Label(left_frame, text="Vehicle History", font=labelfont, bg=lf_bg).place(relx=0.1, rely=0.05)
+Button(left_frame, text='View comparison', font=labelfont, command=lambda: show_image("Chart.png") , width=15).place(relx=0.1, rely=0.35)
+
+
+
+
+# Display image in right_frame
+#Label(right_frame, image=image).grid(row=0,column=0, padx=5, pady=5)
+
 
 # Finalizing the GUI window
 main.update()
