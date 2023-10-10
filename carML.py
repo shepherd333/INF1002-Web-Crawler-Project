776e6fe29081a296420ae75cd2ec687f0f5c7296
Tung Jun Rong Keefe <142516790+junkeefe@users.noreply.github.com>
2023-10-11 00:39:20 +0800

    Update carML.py

--- carML.py
+++ carML.py
@@ -58,3 +58,50 @@ for i,j in zip(cars['mileage'].unique().tolist(),range(cars['mileage'].nunique()
 category_dict={}
 for i,j in zip(cars['type'].unique().tolist(),range(cars['type'].nunique())):
     category_dict[i]=str(j)
+
+#training model & testing samples
+train_x,test_x,train_y,test_y=train_test_split(cars.drop('price',axis=1),cars['price'],test_size=0.2,random_state=89)
+
+#choose the better out of the 2 ensemble algorithm for prediction regression-based task output
+#1. randomforestregressor 
+rfr=RandomForestRegressor(n_estimators=1000,max_depth=15)
+rfr.fit(train_x,train_y)
+predict=rfr.predict(test_x)
+print('MAE',mean_absolute_error(predict,test_y))
+print('R2',r2_score(predict,test_y))
+#expected example output (higher the score the better the accuracy)
+#MAE 1425.9924792171153
+#R2 0.9282483356510569
+
+#2. XGBRegressor 
+xgr=XGBRegressor(n_estimators=1000,learning_rate=0.1,max_depth=5)
+xgr.fit(train_x,train_y)
+predict=xgr.predict(test_x)
+print('MAE',mean_absolute_error(predict,test_y))
+print('R2',r2_score(predict,test_y))
+#expected example output (higher the score the better the accuracy)
+#MAE 1322.161939034648
+#R2 0.9394056654633186
+
+#save the model
+filename = 'trainedcarML.sav'
+pickle.dump(xgr, open(filename, 'wb'))
+with open("model_dictionary.json",'w') as f:
+    json.dump(model_dict,f)
+f.close()
+with open("prices_dictionary.json",'w')as f:
+    json.dump(prices_dict,f)
+f.close()
+with open("depreciation_dictionary.json",'w') as f:
+    json.dump(depr_dict,f)
+f.close()
+with open("registration_dictionary.json",'w')as f:
+    json.dump(reg_dict,f)
+f.close()
+with open("engine_dictionary.json",'w')as f:
+    json.dump(engine_dict,f)
+with open("mileage_dictionary.json",'w')as f:
+    json.dump(dist_dict,f)
+with open("category_dictionary.json",'w')as f:
+    json.dump(category_dict,f)
+f.close()
