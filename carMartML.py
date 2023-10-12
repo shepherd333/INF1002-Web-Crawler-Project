1fdad919b4818c3a025ea5b3a693f148679a8498
junkeefe <142516790+junkeefe@users.noreply.github.com>
2023-10-12 11:58:42 +0800

    Update carMartML.py

--- carMartML.py
+++ carMartML.py
@@ -1,7 +1,5 @@
-import json
 import pandas as pd
 import numpy as np
-import os
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
@@ -26,23 +24,70 @@ data_rm_brand['Registration Date'].dtypes
 #encode Transmission (Auto='1', Manual='0')
 #Define a mapping dictionary
 mapping_dict = {'Auto': 1, 'Manual': 0}
-
 # Apply the mapping to the "Transmission" column
 data_rm_brand['Transmission'] = data_rm_brand['Transmission'].map(mapping_dict)
-
 #Rename the column to 'Transmission' to reflect encoding
 data_rm_brand.rename(columns={'Transmission': 'Transmission'}, inplace=True)
 
 #encode car types
 # Define a mapping dictionary
 mapping_dict2 = {'Hatchback': 0,'Mid-Sized Sedan': 1, 'MPV': 2, 'Luxury Sedan': 3, 'SUV': 4, 'Sports Car': 5}
-
 # Apply the mapping to the "Vehicle Type" column
 data_rm_brand['Vehicle Type'] = data_rm_brand['Vehicle Type'].map(mapping_dict2)
-
 #Rename 'Vehicle Type' to reflect encoding
 data_rm_brand.rename(columns={'Vehicle Type': 'Vehicle Type'}, inplace=True)
 
+#Covert registration date format to encode
+registration_date_datetime = pd.to_datetime(data_rm_brand['Registration Date'], format='%y-%m-%d')
+#Encode the registration date to an integer.
+encoded_registration_date = registration_date_datetime.dt.year * 10000 + registration_date_datetime.dt.month * 100 + registration_date_datetime.dt.day
+
+
+#Corr Matrix
+#data_rm_brand.corr()
+#How each feature relates to price
+#data_rm_brand.corr()['Price'].sort_values(ascending=False)
+#Corr Matrix Heatmap Visualization
+#sns.set(style="white")
+#Generate a mask for the upper triangle
+#mask = np.zeros_like(data_rm_brand.corr(), dtype=np.bool_)
+#mask[np.triu_indices_from(mask)] = True
+#Set up the matplotlib figure to control size of heatmap
+#fig, ax = plt.subplots(figsize=(15,15))
+#Create a custom color palette
+#cmap = \
+#sns.diverging_palette(133, 10, as_cmap=True)  # as_cmap returns a matplotlib colormap object rather than a list of colors
+#Green = Good (low correlation), Red = Bad (high correlation) between the independent variables
+#Plot the heatmap
+#sns.heatmap(data_rm_brand.corr(), mask=mask, annot=True, square=True, cmap=cmap , vmin=-1, vmax=1,ax=ax);
+# Prevent Heatmap Cut-Off Issue
+#bottom, top = ax.get_ylim()
+#ax.set_ylim(bottom + 0.5, top - 0.5)
+
+#Corr Matrix v2.0
+sns.set(style="white")
+# Creating the data
+data = data_rm_brand.corr()
+#Generate a mask for the upper triangle
+mask = np.zeros_like(data, dtype=np.bool_)
+mask[np.triu_indices_from(mask)] = True
+#Set up the matplotlib figure to control size of heatmap
+fig, ax = plt.subplots(figsize=(15,15))
+#Create a custom color palette
+cmap = \
+sns.diverging_palette(133, 10, as_cmap=True)  # as_cmap returns a matplotlib colormap object rather than a list of colors
+#Green = Good (low correlation), Red = Bad (high correlation) between the independent variables
+#Plot the heatmap
+sns.heatmap(data, mask=mask, annot=True, square=True, cmap=cmap , vmin=-1, vmax=1,ax=ax);
+#Prevent Heatmap Cut-Off Issue
+bottom, top = ax.get_ylim()
+ax.set_ylim(bottom + 0.5, top - 0.5)
+
+#Performing a pairplot to visualize the data trends of the variables
+#Show corelation between 2 variables (eg Price VS Depreciation --> negative relationship? Why?)
+sns.pairplot(data_rm_brand);
+
+#BREAKPOINT HERE (until ready to test training)
 #training model & testing samples
 train_x,test_x,train_y,test_y=train_test_split(data.drop('Price',axis=1),data['Price'],test_size=0.2,random_state=89)
 
