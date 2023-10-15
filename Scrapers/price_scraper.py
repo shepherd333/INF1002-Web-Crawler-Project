1a998265292f7ff45fccd4e8dd7db2ba73399b9f
shepherd333 <worldwithoutaking@gmail.com>
2023-10-15 15:54:03 +0800

    edits

--- Scrapers/price_scraper.py
+++ Scrapers/price_scraper.py
@@ -3,21 +3,17 @@ import numpy as np
 
 def price_retrieval(listing_url):
     data_value = listing_url.find_all(class_='font_red')[0].text.strip()
-    data_value = data_value.split('$')
     price = price_error_handling(data_value)
     return price
 
 
 def price_error_handling(data_value):
     try:
-        price = data_value[1]
-        price = int(price.split(',')[0] + price.split(',')[
-            1])
+        price_str = ''.join(filter(str.isdigit, data_value))
 
-    except IndexError:
-        try:
-            price = int(data_value[1])
-        except IndexError:
-            price = np.nan
+        price = int(price_str)
+    except (ValueError, IndexError):
+        price = np.nan
 
     return price
+
